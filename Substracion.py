f3496b4ed98bfc6730eb681f0bd430de7bd332ef
Raedah1 <92187909+Raedah1@users.noreply.github.com>
2021-10-09 00:10:56 +0100

    Update Substracion.py

--- Substracion.py
+++ Substracion.py
@@ -1,10 +1,16 @@
- 
+#In Python 
 def substraction(n_1 , n_2):
     # Using two's complement method
     two_com = (~n_2 + 1)
     substract = n_1 + two_com
 
     return substract 
+   
+
+   
+   
+
+ 
 
 
     
